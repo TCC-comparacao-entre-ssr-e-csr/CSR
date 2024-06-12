d9faacdca2993a5006e5904ed6bfb15fb10dea56
IgorLima <igorlimadrum@gmail.com>
2024-06-12 09:27:59 -0300

    feat: make multiple requests and request time

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,23 +1,54 @@
 import { useEffect, useState } from "react";
 
-export const useFetch = (url, options) => {
+export const useFetch = (inputUrl, options) => {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
+  const [time, setTime] = useState(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      await fetch(url, options)
-        .then((res) => res.json())
-        .then((json) => setData(json))
-        .finally(() => setIsLoading(false))
-        .catch((err) => setError(err));
+    let isMounted = true;
+
+    const fetchData = async (url) => {
+      const startTime = performance.now();
+      try {
+        const response = await fetch(url, options);
+        const json = await response.json();
+        if (isMounted) {
+          setData((prevData) =>
+            Array.isArray(inputUrl) ? [...(prevData || []), json] : json
+          );
+          setTime(performance.now() - startTime);
+        }
+      } catch (err) {
+        if (isMounted) {
+          setError(err);
+          setTime(performance.now() - startTime);
+        }
+      } finally {
+        if (isMounted) {
+          setIsLoading(false);
+        }
+      }
     };
 
-    if (!url) throw new Error("Url is required!");
+    if (!inputUrl) throw new Error("Url is required!");
 
-    fetchData();
-  }, [options, url]);
+    if (Array.isArray(inputUrl)) {
+      inputUrl.forEach((url) => fetchData(url));
+    } else {
+      fetchData(inputUrl);
+    }
 
-  return { isLoading, data, error };
+    return () => {
+      isMounted = false;
+    };
+  }, [JSON.stringify(options), JSON.stringify(inputUrl)]);
+
+  return {
+    isLoading,
+    data,
+    error,
+    time,
+  };
 };
